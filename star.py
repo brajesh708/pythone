3757ebb30807553426c5a47e84be454691afd10d
brajesh mewada <154889477+brajesh708@users.noreply.github.com>
2024-05-24 09:49:57 +0530

    Update star.py

--- star.py
+++ star.py
@@ -1,10 +1,10 @@
 
 
-# n=int(input("enter number"))
-# i=1
-# while (i<=n):
-#     print(i*'*')
-#     i=i+1
+n=int(input("enter number"))
+i=1
+while (i<=n):
+    print(i*'*')
+    i=i+1
 
 
 # n=int(input("enter number"))
@@ -14,38 +14,38 @@
 #     n=n-i
     
     
-# n=int(input("enter number"))
-# i=1
-# while (i<n):
-#     print(i*'*')
-#     i=i+1
-#     while (i<=n)
-# print(n*'*')
-# n=n-i
+n=int(input("enter number"))
+i=1
+while (i<n):
+    print(i*'*')
+    i=i+1
+    while (i<=n)
+print(n*'*')
+n=n-i
     
     
-    # next program 
-    # tribhuj ka code 
-# n=int(input("enter number"))
-# i=1
-# while (i<=n):
-#     print((n-i)*" "+i*"*")
-#     i=i+1
+    next program 
+    tribhuj ka code 
+n=int(input("enter number"))
+i=1
+while (i<=n):
+    print((n-i)*" "+i*"*")
+    i=i+1
     
     
-# n=int(input("enter number"))
-# i=0
-# while (i<n):
-#     print(i*''+(n-i)*'*')
-#     i=i+1
+n=int(input("enter number"))
+i=0
+while (i<n):
+    print(i*''+(n-i)*'*')
+    i=i+1
     
     
-    # trangal banane ka code 
-# n=int(input("enter number"))
-# i=1
-# while (i<=n):
-#     print((n-i)*" "+i*"* ")
-#     i=i+1
+    trangal banane ka code 
+n=int(input("enter number"))
+i=1
+while (i<=n):
+    print((n-i)*" "+i*"* ")
+    i=i+1
 
 
 
