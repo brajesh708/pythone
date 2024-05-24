de162932783c1b302db46e6346db3b9682cab753
brajesh mewada <154889477+brajesh708@users.noreply.github.com>
2024-05-24 09:47:13 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,60 @@
+
+
+# n=int(input("enter number"))
+# i=1
+# while (i<=n):
+#     print(i*'*')
+#     i=i+1
+
+
+# n=int(input("enter number"))
+# i=1
+# while (i<=n):
+#     print(n*'*')
+#     n=n-i
+    
+    
+# n=int(input("enter number"))
+# i=1
+# while (i<n):
+#     print(i*'*')
+#     i=i+1
+#     while (i<=n)
+# print(n*'*')
+# n=n-i
+    
+    
+    # next program 
+    # tribhuj ka code 
+# n=int(input("enter number"))
+# i=1
+# while (i<=n):
+#     print((n-i)*" "+i*"*")
+#     i=i+1
+    
+    
+# n=int(input("enter number"))
+# i=0
+# while (i<n):
+#     print(i*''+(n-i)*'*')
+#     i=i+1
+    
+    
+    # trangal banane ka code 
+# n=int(input("enter number"))
+# i=1
+# while (i<=n):
+#     print((n-i)*" "+i*"* ")
+#     i=i+1
+
+
+
+n=int(input("enter number"))
+i=1
+while (i<=n):
+    print((n-i)*" "+(2*i-1)*'*')
+    i=i+1
+    
+    
+    
+        
