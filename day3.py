1945d19a54cddbd6c45d7d406d9ea04daa8a57f6
brajesh mewada <154889477+brajesh708@users.noreply.github.com>
2024-06-01 17:28:36 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,21 @@
+
+
+
+# x={10,20,23,10,304}
+# print(x)
+
+# x={10,20,23,10,304, 'brajeesh','neeraj'}
+# print(x)
+
+
+x={'rahul','ajey','rohit'}
+print(x)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+
