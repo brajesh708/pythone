214b7afdbf8b271232cb75dcffe81ec72239bb81
brajesh mewada <154889477+brajesh708@users.noreply.github.com>
2024-06-05 21:45:11 +0530

    Add files via upload

--- star.py
+++ star.py
@@ -1,10 +1,10 @@
 
 
-n=int(input("enter number"))
-i=1
-while (i<=n):
-    print(i*'*')
-    i=i+1
+# n=int(input("enter number"))
+# i=1
+# while (i<=n):
+#     print(i*'*')
+#     i=i+1
 
 
 # n=int(input("enter number"))
@@ -14,46 +14,46 @@ while (i<=n):
 #     n=n-i
     
     
-n=int(input("enter number"))
-i=1
-while (i<n):
-    print(i*'*')
-    i=i+1
-    while (i<=n)
-print(n*'*')
-n=n-i
+# n=int(input("enter number"))
+# i=1
+# while (i<n):
+#     print(i*'*')
+#     i=i+1
+#     while (i<=n)
+# print(n*'*')
+# n=n-i
     
     
-    next program 
-    tribhuj ka code 
-n=int(input("enter number"))
-i=1
-while (i<=n):
-    print((n-i)*" "+i*"*")
-    i=i+1
+    # next program 
+    # tribhuj ka code 
+# n=int(input("enter number"))
+# i=1
+# while (i<=n):
+#     print((n-i)*" "+i*"*")
+#     i=i+1
     
     
-n=int(input("enter number"))
-i=0
-while (i<n):
-    print(i*''+(n-i)*'*')
-    i=i+1
+# n=int(input("enter number"))
+# i=0
+# while (i<n):
+#     print(i*''+(n-i)*'*')
+#     i=i+1
     
     
-    trangal banane ka code 
-n=int(input("enter number"))
-i=1
-while (i<=n):
-    print((n-i)*" "+i*"* ")
-    i=i+1
+    # trangal banane ka code 
+# n=int(input("enter number"))
+# i=1
+# while (i<=n):
+#     print((n-i)*" "+i*"* ")
+#     i=i+1
 
 
 
-n=int(input("enter number"))
-i=1
-while (i<=n):
-    print((n-i)*" "+(2*i-1)*'*')
-    i=i+1
+# n=int(input("enter number"))
+# i=1
+# while (i<=n):
+#     print((n-i)*" "+(2*i-1)*'*')
+#     i=i+1
     
     
     
